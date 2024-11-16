88b8874c19782745a5b0b4dc67524e10c00ba6b4
GloriousHopeGC <cuyosglorious@gmail.com>
2024-11-16 15:50:31 +0800

    dropdown modify

--- public/lib/js/my.js
+++ public/lib/js/my.js
@@ -105,23 +105,35 @@ $(document).ready(function() {
             } else {
                 $('#userData').html(`
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container">
-                        <a class="navbar-brand" href="update_user.php">${response.user_info.name}</a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                                <div class="collapse navbar-collapse" id="navbarNav">
-                                    <ul class="navbar-nav ms-auto">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#" onclick="confirmLogout()">Logout</a> <!-- Updated Logout link -->
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                </nav>
+                <div class="container">
+                    <a class="navbar-brand" href="update_user.php">${response.user_info.name}</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                            </li>
+                            <!----<li class="nav-item">
+                                <a class="nav-link" href="#" onclick="confirmLogout()">Logout</a>
+                            </li> -->
+                            <!-- Dropdown Menu -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../lib/images/user_profile/${response.user_info.image_name}" alt="Profile" class="rounded-circle" style="width: 30px; height: 30px;">
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="update_user.php">Profile</a></li>
+                                    <!----<li><a class="dropdown-item" href="#">Account Settings</a></li>
+                                    <li><a class="dropdown-item" href="#">Privacy</a></li> -->
+                                    <li><a class="dropdown-item" href="#" onclick="confirmLogout()">Log-out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
                 `);
                 $('#userInfo').html(`
                    <div class="container mt-5">
@@ -349,13 +361,25 @@ $(document).ready(function() {
                                         <li class="nav-item">
                                             <a class="nav-link active" aria-current="page" href="admin.php">Home</a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="logout.php">Logout</a> <!-- Updated Logout link -->
-                                        </li>
-                                    </ul>
+                                        <!-- Updated Logout link -->
+                                        <!--<li class="nav-item">
+                                            <a class="nav-link" href="logout.php">Logout</a> 
+                                        </li> -->
+                                         <!-- Dropdown Menu -->
+                                        <li class="nav-item dropdown">
+                                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                <img src="../lib/images/user_profile/${response.user_info.image_name}" alt="Profile" class="rounded-circle" style="width: 30px; height: 30px;">
+                                            </a>
+                                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                    <li><a class="dropdown-item" href="update_admin.php">Profile</a></li>
+                                                    <!----<li><a class="dropdown-item" href="#">Account Settings</a></li>
+                                                    <li><a class="dropdown-item" href="#">Privacy</a></li> -->
+                                                    <li><a class="dropdown-item" href="#" onclick="confirmLogout()">Log-out</a></li>
+                                                </ul>
+                                        </ul>
                                 </div>
                             </div>
-                </nav>
+                    </nav>
                 `);
                 $('#userAdminInfo').html(`
                    <div class="container mt-5">
--- public/view/update_admin.php
+++ public/view/update_admin.php
@@ -10,6 +10,7 @@ if (isset($userData['error'])) {
 }
 $user = $userData['user'];
 $user_info = $userData['user_info'];
+$controller->handleLogoutAction(); 
 ?>
 
 <!DOCTYPE html>
@@ -23,8 +24,8 @@ $user_info = $userData['user_info'];
     <script defer src="/edma/public/lib/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" href="/edma/public/lib/images/AGCOWC TRANSPARENT.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script> -->
     <link rel="stylesheet" href="/edma/public/lib/css/edit.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="/edma/public/lib/js/my.js"></script>
